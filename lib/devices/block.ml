420e9a8063be8bfd0af2ad971a31d438f9e11eba
Virgile Robles <virgile.robles@protonmail.ch>
2024-10-23 18:10:28 +0200

    use unix blocks for unikraft

--- lib/devices/block.ml
+++ lib/devices/block.ml
@@ -76,10 +76,8 @@ let block_conf file =
     | #Key.mode_xen -> xen_block_packages
     | #Key.mode_solo5 ->
         [ package ~min:"0.8.0" ~max:"0.9.0" "mirage-block-solo5" ]
-    | #Key.mode_unix ->
+    | #Key.mode_unix | #Key.mode_unikraft ->
         [ package ~min:"2.12.0" ~max:"3.0.0" "mirage-block-unix" ]
-    | #Key.mode_unikraft ->
-        [ package ~min:"0.0.1" ~max:"0.0.1" "mirage-block-unikraft" ]
   in
   let configure _ =
     let (_ : block_t) = make_block_t file in
